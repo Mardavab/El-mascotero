b2af46aa29af5eea84adfacea31e33f0411b5f02
Mardavab <mardavab@gmail.com>
2024-03-19 08:13:20 -0500

    Todo

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function remplazar() {
+
+    let textoReemplazado = "Cuidar a nuestras mascotas es más que una responsabilidad; es un acto de amor y compromiso que enriquece nuestras vidas y las de nuestros fieles compañeros. Los perros, con su lealtad inquebrantable y su capacidad para alegrar incluso los días más sombríos, merecen nuestra atención y cuidado constante. Desde los juegos en el parque hasta las siestas juntos en el sofá, cada momento compartido fortalece el vínculo especial que compartimos con nuestros amigos peludos. Al brindarles un hogar seguro, amor incondicional y cuidados adecuados, no solo les damos una vida plena, sino que también nos enriquecemos a nosotros mismos con su compañía y afecto.";
+    document.getElementById("texto").innerHTML = textoReemplazado;
+
+}
+function cambiarColor() {
+    let color = "#000000";
+    document.body.style.backgroundColor = color;
+}
